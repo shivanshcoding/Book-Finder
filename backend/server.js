7ba9ac9c461769f5fde69dc82d896d80dff5fef6
shivanshcoding <shivanshcoding2005@gmail.com>
2025-09-30 17:16:07 +0530

    Improvements in server.js

--- backend/server.js
+++ backend/server.js
@@ -8,17 +8,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Example route
-app.get("/", (req, res) => {
-  res.send("Express backend is running!");
-});
-
-// Example API endpoint (for books if needed)
-app.get("/api/books", (req, res) => {
-  const query = req.query.title;
-  res.json({ message: `You searched for "${query}"` });
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
